c266f37069fcef83ef12296356de30c88c889c46
Unknown <yhyn37@naver.com>
2018-10-29 11:34:15 +0900

    검색화면에서 상세 표시화면 추가

--- app/src/main/java/com/example/iclab/st/ValueprintActivity.java
+++ app/src/main/java/com/example/iclab/st/ValueprintActivity.java
@@ -15,15 +15,22 @@ import java.util.List;
 
 import static com.example.iclab.st.NamesrchActivity.newCS;
 import static com.example.iclab.st.NewplaceActivity.GCSurvey;
+import static com.example.iclab.st.SurveyActivity.frameId;
+import static com.example.iclab.st.SurveyActivity.plateId;
 
 public class ValueprintActivity extends AppCompatActivity {
     int pos = -1;
     static public boolean is_appended = false;
+    boolean detailCheck=false;
+    Button detail;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_valueprint);
         Button appendButton=findViewById(R.id.appendButton);
+        detail=findViewById(R.id.list_detail);
+
+
         appendButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,7 +45,7 @@ public class ValueprintActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         final ListView vList = findViewById(R.id.valueList);
-        List<String> listinfo=new ArrayList<>();
+        final List<String> listinfo=new ArrayList<>();
         final ArrayAdapter<String> listAdapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, listinfo);
 
 
@@ -46,6 +53,96 @@ public class ValueprintActivity extends AppCompatActivity {
         {
              pos = intent.getIntExtra("position",-1);
         }
+
+
+        detail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                listinfo.clear();
+                detailCheck=!detailCheck;
+
+               if(detailCheck){
+                   CSurvey tmp =newCS.get(pos);
+                   detail.setText("기본");
+//
+                   ArrayList<String> list = new ArrayList<>();
+                   ArrayList<Integer> list1 = new ArrayList<>();
+                   ArrayList<String> list3 = new ArrayList<>();
+                   ArrayList<Integer> list2 = new ArrayList<>();
+
+                   for(int i=0;i<tmp.list.size();i++)
+                   {
+                       if(!list.contains(tmp.list.get(i).plate_id))
+                       {
+                           list.add(tmp.list.get(i).plate_id);
+                           list1.add(1);
+                       }
+                       else
+                       {
+                           int ch=-1;
+                           for(int k=0;k<list.size();k++)
+                               if(list.get(k).equals(tmp.list.get(i).plate_id))
+                                   ch=k;
+                           list1.set(ch,list1.get(ch)+1);
+                       }
+                       if(tmp.list.get(i).framecheck)
+                       {
+                           int ch =-1;
+                           for(int k=0;k<plateId.size();k++) {
+
+                               if(plateId.get(k).contains(tmp.list.get(i).plate_id))
+                                   ch=k;
+                           }
+                           if(!list3.contains(frameId.get(ch))) {
+                               list3.add(frameId.get(ch));
+                               list2.add(1);
+                           }
+                           else {
+                               int ch2=-1;
+                               for(int j=0;j<list3.size();j++)
+                                   if(list3.get(j).equals(frameId.get(ch)))
+                                       ch2=j;
+                               list2.set(ch2,list2.get(ch2)+1);
+                           }
+
+                       }
+
+                   }
+                   String longstr ="";
+                   for(int i =0;i<list1.size();i++)
+                       longstr += list.get(i) +" : " +list1.get(i)+" 조\n";
+                   longstr += "\n";
+                   for(int i =0;i<list2.size();i++)
+                       longstr += list3.get(i) +" : " +list2.get(i)+" 조\n";
+
+
+
+                   //
+
+                   listinfo.add(longstr);
+
+                }
+                else{
+                   detail.setText("상세");
+                   for(int i=0; i< newCS.get(pos).list.size();i++)
+                   {
+                       SurveyList sl = newCS.get(pos).list.get(i);
+//            String s = "NO."+(i+1)+ "   보호판 : "+sl.plate_id +"\n위도 : "+sl.latitude+"  \n경도 : "+sl.longitude;
+                       String firstData="";
+                       String pointSum="";
+                       for(int j=0;j<sl.points.length;j++)
+                           pointSum+=sl.points[j]+"  ";
+
+                       firstData += "No. " + (i + 1) + "\n보호판 이름: " + sl.plate_id + "\n 뿌리 값: " + pointSum+"\n 메모 : "+sl.memo;// 뭐 적어주지.
+
+                       listinfo.add(firstData);
+                   }
+                }
+
+                vList.setAdapter(listAdapter);
+            }
+        });
+
         for(int i=0; i< newCS.get(pos).list.size();i++)
         {
             SurveyList sl = newCS.get(pos).list.get(i);
@@ -55,7 +152,7 @@ public class ValueprintActivity extends AppCompatActivity {
             for(int j=0;j<sl.points.length;j++)
                 pointSum+=sl.points[j]+"  ";
 
-            firstData += "No. " + (i + 1) + "\n보호판 이름: " + sl.plate_id + "\n 뿌리 값: " + pointSum;// 마지막 페이지 출력문
+            firstData += "No. " + (i + 1) + "\n보호판 이름: " + sl.plate_id + "\n 뿌리 값: " + pointSum+"\n 메모 : "+sl.memo;// 뭐 적어주지.
 
             listinfo.add(firstData);
         }
